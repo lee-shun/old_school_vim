2f848a374a6dbec156300725de7f4965a16a27cb
lee-shun <lee970802@163.com>
2022-02-07 22:51:35 -0500

    u

--- font/install_nerd_font.sh
+++ font/install_nerd_font.sh
@@ -19,3 +19,4 @@
 
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+fc-cache -fv
