7f92a738d623f6609a87d832599f6e8fdabe4594
lee-shun <lee970802@163.com>
2022-08-27 18:39:14 -0400

    u

--- env/config_env.sh
+++ env/config_env.sh
@@ -31,3 +31,6 @@ sudo apt install python3 python3-pip
 sudo pip3 install pynvim
 
 echo "install nerd font for the system..."
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+fc-cache -fv
