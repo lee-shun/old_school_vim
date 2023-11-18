058dca37871bedf86c122a7da67eabbbc2337686
Shun Li <shun.li.at.casia@outlook.com>
2023-11-18 11:25:17 +0800

    u

---
+++ env/install_nerd_fonts_mirror.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# ------------------------------------------------------------------------------
+#
+#   Copyright (C) 2022 Concordia NAVlab. All rights reserved.
+#
+#   @Filename: install_nerd_fonts_mirror.sh
+#
+#   @Author: Shun Li
+#
+#   @Date: 2022-09-01
+#
+#   @Email: 2015097272@qq.com
+#
+#   @Description:
+#
+# ------------------------------------------------------------------------------
+
+sudo rm -rf /usr/share/fonts/osv_font
+sudo mkdir -p /usr/share/fonts/osv_font
+git clone https://gitee.com/old_school_vim/nerd_fonts --depth 1
+
+cd nerd_fonts/
+rm -rf .git/
+unzip Hasklig.zip -d osv_font/
+unzip MapleMono-SC-NF.zip -d osv_font/
+sudo mv osv_font /usr/share/fonts/
+cd ..
+
+sudo fc-cache -fv
